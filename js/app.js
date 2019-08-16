22815cc5076df1dda37b1abe6e8c43fd43e71367
John Prescott <jprescottirl@gmail.com>
2019-08-15 19:38:05 -0700

    array with quotes and starting the function.

---
+++ js/app.js
@@ -0,0 +1,53 @@
+
+/***
+  Create the `getRandomQuote` function to:
+   - Create a variable to store a random number 
+   - Cse the random number to `return` a random quote object from the `quotes` array.
+***/
+
+
+
+function getRandomQuote(array) {
+     var randomQu = Math.floor(Math.random() * quotes.length);
+    return array;
+}
+
+alert();
+
+
+/***
+  Create the `printQuote` function to: 
+   - Call the `getRandomQuote` function and assign it to a variable.
+   - Create a variable for the HTML string and set it equal to an empty string.
+   - Use the HTML template in the instructions or the markup in the index.html file, AND 
+     the random quote vairable to build your HTML string.
+   - Add the quote and source section to the HTML string.
+   - Use an if statement to check for the citation property before adding it to the HTML string.
+   - Use an if statement to check for the year property before adding it to the HTML string.
+   - Don't forget to close that final `p` tag.
+   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
+***/
+
+var HTML = '';
+
+
+function printQuote() {
+    
+   
+
+}
+
+
+
+
+/***
+  When the "Show another quote" button is clicked, the event listener 
+  below will be triggered, and it will call, or "invoke", the `printQuote` 
+  function. So do not make any changes to the line of code below this 
+  comment.
+***/
+
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+
+// Remember to delete the comments that came with this file, and replace them with your own code comments.
---
+++ js/script.js
@@ -0,0 +1,48 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
+
+
+
+var quotes = [
+    {
+      quote: "Intelligence is the ability to adapt to change.",
+      source: '-Stephen Hawking'
+    },
+
+    {
+      quote: "Details matter, it's worth waiting to get it right.",
+      source: '-Steven Jobs'
+    },
+
+    {
+      quote: "The way to get started is to quit talking and begin doing.",
+      source: '-Walt Disney'
+    },
+
+    {
+      quote: "All we have to decide is what to do with the time that is given us.",
+      source: '-Gandalf The Grey',
+      citation: 'The lord of the rings - The fellowship of the ring'
+    },
+
+    {
+      quote: "A date which will live in infamy.",
+      source: '-Franklin D. Roosevelt',
+      year: 'December 8, 1941.'
+    }
+
+
+
+];
+
+console.log(quotes);
+
+
+
+
+
+
